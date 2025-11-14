497fd29d666b078c28e70fd87e0eac20938c5b27
ayoubMah <ayoubelmahjouby9@gmail.com>
2025-11-14 15:53:21 +0100

    refact: new structure to support microservices and distributed systems

---
+++ appointment-service/src/test/java/episante/aai/appointmentservice/AppointmentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package episante.aai.appointmentservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppointmentServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ patient-service/src/main/java/episante/aai/patientservice/PatientServiceApplication.java
@@ -0,0 +1,13 @@
+package episante.aai.patientservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatientServiceApplication.class, args);
+    }
+
+}
